9515b295e27b1de0a89412a13a0dfa4f12079c0d
RAFI ALIF <rafialif2907@gmail.com>
2022-04-09 12:03:37 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ modul7_1302204116/BankTransferConfig.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul7_1302204116
+{
+    class BankTransferConfig
+    {
+        public dynamic ReadJson()
+        {
+            string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            string fileName = "/bank_transfer_config.json";
+            string jsonString = File.ReadAllText(path + fileName);
+
+            dynamic conf = JsonConvert.DeserializeObject(jsonString);
+            return conf;
+        }
+    }
+}
--- modul7_1302204116/Program.cs
+++ modul7_1302204116/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,81 @@ namespace modul7_1302204116
     {
         static void Main(string[] args)
         {
+            BankTransferConfig ConfigBankTransfer = new BankTransferConfig();
+            dynamic conf = ConfigBankTransfer.ReadJson();
+
+            if (conf.lang == "en")
+            {
+                Console.WriteLine("Please insert the amount of money to transfer : ");
+            }
+            else if (conf.lang == "id")
+            {
+                Console.WriteLine("Masukkan jumlah uang yang akan di-transfer : ");
+            }
+
+            string TransferUangStr = Console.ReadLine();
+            int TransferUangInt = int.Parse(TransferUangStr);
+
+            int FeeTransfer;
+            if (TransferUangInt <= (int)conf.transfer.threshold)
+            {
+                FeeTransfer = conf.transfer.low_fee;
+            }
+            else
+            {
+                FeeTransfer = conf.transfer.high_fee;
+            }
+
+            if (conf.lang == "en")
+            {
+                Console.WriteLine("Transfer fee = " + FeeTransfer);
+                Console.WriteLine("Total amount = " + (FeeTransfer + TransferUangInt) );
+                Console.WriteLine("Select transfer method");
+            }
+            else if (conf.lang == "id")
+            {
+                Console.WriteLine("Biaya transfer = " + FeeTransfer);
+                Console.WriteLine("Total biaya = " + (FeeTransfer + TransferUangInt) );
+                Console.WriteLine("Pilih metode transfer");
+            }
+
+            int index = 0;
+            foreach (var metode in conf.methods)
+            {
+                index++;
+                Console.WriteLine(index + ". " + metode);
+            }
+
+            Console.WriteLine();
+
+            string confirm;
+            if (conf.lang == "en")
+            {
+                Console.WriteLine("Please type '" + conf.confirmation.en + "' to confirm the transaction:");
+                confirm = Console.ReadLine();
+                if (confirm == (string)conf.confirmation.en)
+                {
+                    Console.WriteLine("The transfer is completed");
+                }
+                else
+                {
+                    Console.WriteLine("Transfer is cancelled");
+                }
+            }
+            else if (conf.lang == "id")
+            {
+                Console.WriteLine("Tolong ketik '" + conf.confirmation.id + "' untuk mengkonfirmasi transaksi:");
+                confirm = Console.ReadLine();
+                if (confirm == (string)conf.confirmation.id)
+                {
+                    Console.WriteLine("Proses transfer berhasil");
+                }
+                else
+                {
+                    Console.WriteLine("Transfer dibatalkan");
+                }
+            }
+
 
         }
     }
